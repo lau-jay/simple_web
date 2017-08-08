35b1dc9e61ea3fd154f35f188e7c9b6848a7d301
pyclearl <cappyclear@gmail.com>
2017-08-08 17:32:13 +0800

    add refactored

---
+++ code/05-refactored/time.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+# author: pyclearl
+
+from datetime import datetime
+print('''
+<html>
+   <body>
+     <p>
+        Generated {0}
+     </p>
+   </body>
+</html>
+'''.format(datetime.now()))
